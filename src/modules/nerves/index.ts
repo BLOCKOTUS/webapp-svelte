7d0f20b9912ff9e307d7d32e86a65a1a05313a99
Daniel Febrero <febrero@me.com>
2020-11-07 19:02:23 +0100

    BLOCKOTUS/organism#84 document functions modules/nerves

--- src/modules/nerves/index.ts
+++ src/modules/nerves/index.ts
@@ -2,11 +2,18 @@ import axios, { AxiosPromise } from 'axios';
 
 import type { Wallet } from '@@Modules/user';
 
+/**
+ * Minimum object properties always returned by the nerves from the network when submnitting a request.
+ */
 export type RequestReponseObject = { 
   success: boolean; 
   message: string;
 };
 
+/**
+* Prepare and submit a request to the nerves -> network.
+* and return the response: network -> nerves -> ui
+*/
 export const request = ({
   username = '',
   wallet,
